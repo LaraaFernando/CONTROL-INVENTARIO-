219a93e51e8fc6c2fb8fc09fcc389f09dd37f2ad
LaraaFernando <messi020306@gmail.com>
2026-08-23 02:03:48 -0600

    Montar Pulso del negocio en Inicio

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AdvancedReceivingExperience from "./advanced-receiving-experience";
 import BlockOneExperience from "./block-one-experience";
 import BlockOneNavigationFix from "./block-one-navigation-fix";
 import BlockTwoExperience from "./block-two-experience";
+import BusinessInsightsExperience from "./business-insights-experience";
 import CommercialMovementExperience from "./commercial-movement-experience";
 import InventoryApp from "./inventory-app";
 import InventoryQuickSearchExperience from "./inventory-quick-search-experience";
@@ -17,5 +18,5 @@ import VisualCodeReaderExperience from "./visual-code-reader-experience";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return <><InventoryApp /><SalesExperience /><BlockOneExperience /><BlockOneNavigationFix /><BlockTwoExperience /><AdvancedReceivingExperience /><ProductCommercialExperience /><CommercialMovementExperience /><PurchaseOrderCommercialExperience /><InventoryQuickSearchExperience /><VisualCodeReaderExperience /><ReplenishmentExperience /><TransactionSafetyExperience /><SaleCancellationExperience /><ProductSuppliersExperience /></>;
+  return <><InventoryApp /><SalesExperience /><BlockOneExperience /><BlockOneNavigationFix /><BlockTwoExperience /><AdvancedReceivingExperience /><ProductCommercialExperience /><CommercialMovementExperience /><PurchaseOrderCommercialExperience /><InventoryQuickSearchExperience /><VisualCodeReaderExperience /><ReplenishmentExperience /><BusinessInsightsExperience /><TransactionSafetyExperience /><SaleCancellationExperience /><ProductSuppliersExperience /></>;
 }
