50eb36371dc590b7893013f4830e0e57cacfc82a
LaraaFernando <messi020306@gmail.com>
2026-08-22 21:05:06 -0600

    Activar reparación de navegación del Bloque 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import BlockOneExperience from "./block-one-experience";
+import BlockOneNavigationFix from "./block-one-navigation-fix";
 import InventoryApp from "./inventory-app";
 import SalesExperience from "./sales-experience";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return <><InventoryApp /><SalesExperience /><BlockOneExperience /></>;
+  return <><InventoryApp /><SalesExperience /><BlockOneExperience /><BlockOneNavigationFix /></>;
 }
