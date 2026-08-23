5a165607b677325d9e526e4ed55f8877f4ccad1a
LaraaFernando <messi020306@gmail.com>
2026-08-22 23:47:13 -0600

    Montar experiencia de reabastecimiento

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,12 @@ import InventoryApp from "./inventory-app";
 import InventoryQuickSearchExperience from "./inventory-quick-search-experience";
 import ProductCommercialExperience from "./product-commercial-experience";
 import PurchaseOrderCommercialExperience from "./purchase-order-commercial-experience";
+import ReplenishmentExperience from "./replenishment-experience";
 import SalesExperience from "./sales-experience";
 import VisualCodeReaderExperience from "./visual-code-reader-experience";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return <><InventoryApp /><SalesExperience /><BlockOneExperience /><BlockOneNavigationFix /><BlockTwoExperience /><AdvancedReceivingExperience /><ProductCommercialExperience /><CommercialMovementExperience /><PurchaseOrderCommercialExperience /><InventoryQuickSearchExperience /><VisualCodeReaderExperience /></>;
+  return <><InventoryApp /><SalesExperience /><BlockOneExperience /><BlockOneNavigationFix /><BlockTwoExperience /><AdvancedReceivingExperience /><ProductCommercialExperience /><CommercialMovementExperience /><PurchaseOrderCommercialExperience /><InventoryQuickSearchExperience /><VisualCodeReaderExperience /><ReplenishmentExperience /></>;
 }
