97b4413873fa3def31a3c6c6d3c0dbfe388b2461
LaraaFernando <messi020306@gmail.com>
2026-08-22 22:34:39 -0600

    Integrar búsqueda rápida de inventario

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import BlockOneNavigationFix from "./block-one-navigation-fix";
 import BlockTwoExperience from "./block-two-experience";
 import CommercialMovementExperience from "./commercial-movement-experience";
 import InventoryApp from "./inventory-app";
+import InventoryQuickSearchExperience from "./inventory-quick-search-experience";
 import ProductCommercialExperience from "./product-commercial-experience";
 import PurchaseOrderCommercialExperience from "./purchase-order-commercial-experience";
 import SalesExperience from "./sales-experience";
@@ -11,5 +12,5 @@ import SalesExperience from "./sales-experience";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return <><InventoryApp /><SalesExperience /><BlockOneExperience /><BlockOneNavigationFix /><BlockTwoExperience /><AdvancedReceivingExperience /><ProductCommercialExperience /><CommercialMovementExperience /><PurchaseOrderCommercialExperience /></>;
+  return <><InventoryApp /><SalesExperience /><BlockOneExperience /><BlockOneNavigationFix /><BlockTwoExperience /><AdvancedReceivingExperience /><ProductCommercialExperience /><CommercialMovementExperience /><PurchaseOrderCommercialExperience /><InventoryQuickSearchExperience /></>;
 }
