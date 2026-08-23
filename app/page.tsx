ec8f156c466be3a1ae608406d4b80dfde101a1af
LaraaFernando <messi020306@gmail.com>
2026-08-22 20:03:16 -0600

    Activar experiencia de venta multiproducto

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import InventoryApp from "./inventory-app";
+import SalesExperience from "./sales-experience";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return <InventoryApp />;
+  return <><InventoryApp /><SalesExperience /></>;
 }
